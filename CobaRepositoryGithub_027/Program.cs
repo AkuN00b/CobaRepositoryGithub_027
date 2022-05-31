9923fb41744847591f7e1265177ab2acf2d6c22a
AkuN00b <gerlandocorputty@gmail.com>
2022-05-31 15:20:07 +0700

    Add project files.

---
+++ CobaRepositoryGithub_027/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CobaRepositoryGithub_027
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
